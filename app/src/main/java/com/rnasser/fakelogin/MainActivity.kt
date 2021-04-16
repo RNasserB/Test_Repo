e7b93698ba9c0c48abbee0c2f9202e95ad3c136c
RNasserB <rnbprojetos@gmail.com>
2021-04-15 21:59:11 -0300

    Criando Lógica

--- app/src/main/java/com/rnasser/fakelogin/MainActivity.kt
+++ app/src/main/java/com/rnasser/fakelogin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rnasser.fakelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val init = 1
+        if (init == 1){
+            Toast.makeText(this, "True", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
